d355c25ea97b81bf8ed807f10cf0c7fa90e5b6ab
João Pedro Miranda <jaopedhrro2@gmail.com>
2025-12-23 14:24:06 -0300

    Create 04_etl_fato.sql with ETL comments
    Add initial comments for ETL process in 04_etl_fato.sql

---
+++ sql/04_etl_fato.sql
@@ -0,0 +1,6 @@
+-- 04_etl_fato.sql
+-- ETL das tabelas fato
+-- - Carga de fato_lancamento usando lancamentos_raw + dimensões
+-- - Carga de fato_conta usando contas_raw + dimensões
+
+-- (nas próximas etapas vamos escrever os INSERT...SELECT aqui)
