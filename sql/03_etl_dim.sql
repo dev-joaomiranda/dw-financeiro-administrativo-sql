a5db451e33d9e31098eec6840963e931bea18e89
João Pedro Miranda <jaopedhrro2@gmail.com>
2025-12-23 14:23:30 -0300

    Create 03_etl_dim.sql for ETL processes
    Add ETL script for dimension tables

---
+++ sql/03_etl_dim.sql
@@ -0,0 +1,8 @@
+-- 03_etl_dim.sql
+-- ETL das dimensões
+-- - Geração do calendário em dim_tempo
+-- - Carga de dim_centro_custo a partir de centros_custo_raw
+-- - Criação de dim_categoria_financeira a partir das categorias das tabelas RAW
+-- - Criação de dim_pessoa a partir de contas_raw
+
+-- (nas próximas etapas vamos escrever os INSERT...SELECT aqui)
