622968a9e535f8056f8db70f8b348b686a5b4546
João Pedro Miranda <jaopedhrro2@gmail.com>
2025-12-23 14:22:59 -0300

    Create initial SQL file for Data Warehouse schema
    Added initial comments for schema creation.

---
+++ sql/02_schema_dim_fato.sql
@@ -0,0 +1,6 @@
+-- 02_schema_dim_fato.sql
+-- Criação das dimensões e das tabelas fato do Data Warehouse
+-- dim_tempo, dim_centro_custo, dim_categoria_financeira, dim_pessoa
+-- fato_lancamento, fato_conta
+
+-- (nas próximas etapas vamos preencher os CREATE TABLE aqui)
