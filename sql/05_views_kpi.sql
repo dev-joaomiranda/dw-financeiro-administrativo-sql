b936c56b0306a5dcb9502a4e0ad14dadcae7d6cb
João Pedro Miranda <jaopedhrro2@gmail.com>
2025-12-23 14:24:40 -0300

    Create 05_views_kpi.sql for KPI views
    Add SQL file for KPI report views with examples.

---
+++ sql/05_views_kpi.sql
@@ -0,0 +1,8 @@
+-- 05_views_kpi.sql
+-- Views de relatórios (camada de KPIs para BI)
+-- Exemplos:
+-- - vw_fluxo_caixa_mensal
+-- - vw_despesa_por_centro
+-- - vw_aging_contas
+
+-- (nas próximas etapas vamos criar as VIEW aqui)
